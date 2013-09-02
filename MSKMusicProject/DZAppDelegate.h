29162e4b62deccb0b2f6a0aeeaa41e9ffa30cf23
msk <78728930@qq.com>
2013-09-02 16:37:07 +0800

    Initial Commit

---
+++ MSKMusicProject/DZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DZAppDelegate.h
+//  MSKMusicProject
+//
+//  Created by msk on 13-9-2.
+//  Copyright (c) 2013年 AHDZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MSKMusicProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MSKMusicProject
+//
+//  Created by msk on 13-9-2.
+//  Copyright (c) 2013年 AHDZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DZAppDelegate class]));
+	}
+}
